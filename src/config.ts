fcbd87368c7c53292e8a26869cec27f0157f07a7
unknown <pranavkulkarni33917@gmail.com>
2025-09-21 22:51:59 +0530

    config file backend url

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,2 @@
 // export const BACKEND_URL = "http://localhost:4000";
-export const BACKEND_URL = "https://sweet-escape-backend.onrender.com";
+export const BACKEND_URL = "https://sweet-escape-backend.vercel.app";
